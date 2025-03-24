1e15eaeab35239228c89e2cc56c7b9a8f2126965
Davi Tavares <dxrktavares3@gmail.com>
2025-03-24 09:04:13 -0300

    Adiciona aula 4 sobre laços de repetição

---
+++ ExercicioAulaJava/src/ExercicioAulaJava_4/Enquanto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package ExercicioAulaJava_4;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Enquanto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Digite um numero: ");
+        int numero = teclado.nextInt();
+        
+        while (numero<=10)
+        {
+            System.out.println("O numero é: "+numero++);
+            
+        }
+    }
+}
