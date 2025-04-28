28b4c3cb1d412e04404112f394d7d17cdf332595
Davi Tavares <dxrktavares3@gmail.com>
2025-04-28 08:50:45 -0300

    Adiciona exercicios relacionados a aula 05 de matriz e vetores

---
+++ Ds-davi-tavares/src/ExercicioAulaJava_5/Exercicio_1_facil.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package ExercicioAulaJava_5;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Exercicio_1_facil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int[] Numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Os 10 numeros são: ");
+        
+        for (int x = 0; x < Numbers.length ; x++){
+            System.out.println(Numbers[x]);
+        }
+    }
+}
+
