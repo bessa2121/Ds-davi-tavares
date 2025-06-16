ae485fdc325f26c1407798412691181caa8751b1
Davi Tavares <dxrktavares3@gmail.com>
2025-06-16 18:27:19 -0300

    Adiciona OptionPane aos arquivos sobre carro

--- Ds-davi-tavares/src/POO/Car.java
+++ Ds-davi-tavares/src/POO/Car.java
@@ -4,12 +4,8 @@
  */
 package POO;
 
-/**
- *
- * @author DAVI
- */
+import javax.swing.JOptionPane;
 
-// Base class Car
 public class Car {
     protected String brand;
     protected String model;
@@ -21,7 +17,6 @@ public class Car {
     protected boolean maintenanceNeeded;
     protected String color;
 
-    // Constructor
     public Car(String brand, String model, int year, String color) {
         this.brand = brand;
         this.model = model;
@@ -29,132 +24,128 @@ public class Car {
         this.color = color;
         this.isOn = false;
         this.speed = 0;
-        this.fuelLevel = 100; // Full fuel tank
+        this.fuelLevel = 100;
         this.mileage = 0;
         this.maintenanceNeeded = false;
     }
 
-    // Method to turn on the car
     public void turnOn() {
         if (!isOn) {
             isOn = true;
-            System.out.println("The car is now on.");
+            JOptionPane.showMessageDialog(null, "The car is now on.");
         } else {
-            System.out.println("The car is already on.");
+            JOptionPane.showMessageDialog(null, "The car is already on.");
         }
     }
 
-    // Method to turn off the car
     public void turnOff() {
         if (isOn) {
             isOn = false;
             speed = 0;
-            System.out.println("The car is now off.");
+            JOptionPane.showMessageDialog(null, "The car is now off.");
         } else {
-            System.out.println("The car is already off.");
+            JOptionPane.showMessageDialog(null, "The car is already off.");
         }
     }
 
-    // Method to accelerate
     public void accelerate(int increment) {
         if (isOn) {
             if (fuelLevel > 0) {
                 speed += increment;
                 fuelLevel -= increment / 2;
                 mileage += increment / 5;
-                System.out.println("Current speed: " + speed + " km/h");
+                JOptionPane.showMessageDialog(null, "Current speed: " + speed + " km/h");
                 checkMaintenance();
             } else {
-                System.out.println("Out of fuel! Please refuel.");
+                JOptionPane.showMessageDialog(null, "Out of fuel! Please refuel.");
             }
         } else {
-            System.out.println("The car is off! Turn it on first.");
+            JOptionPane.showMessageDialog(null, "The car is off! Turn it on first.");
         }
     }
 
-    // Method to brake
     public void brake() {
         if (speed > 0) {
             speed -= 10;
             if (speed < 0) speed = 0;
-            System.out.println("Braking... Current speed: " + speed + " km/h");
+            JOptionPane.showMessageDialog(null, "Braking... Current speed: " + speed + " km/h");
         } else {
-            System.out.println("The car is already stopped.");
+            JOptionPane.showMessageDialog(null, "The car is already stopped.");
         }
     }
 
-    // Method to refuel
     public void refuel(int amount) {
         fuelLevel += amount;
         if (fuelLevel > 100) fuelLevel = 100;
-        System.out.println("Fuel refilled. Current level: " + fuelLevel + "%");
+        JOptionPane.showMessageDialog(null, "Fuel refilled. Current level: " + fuelLevel + "%");
     }
 
-    // Method to check maintenance status
     private void checkMaintenance() {
         if (mileage >= 5000) {
             maintenanceNeeded = true;
-            System.out.println("Warning: The car needs maintenance!");
+            JOptionPane.showMessageDialog(null, "Warning: The car needs maintenance!");
         }
     }
 
-    // Method to perform maintenance
     public void performMaintenance() {
         maintenanceNeeded = false;
         mileage = 0;
-        System.out.println("Maintenance completed. The car is ready to drive!");
+        JOptionPane.showMessageDialog(null, "Maintenance completed. The car is ready to drive!");
     }
 
-    // Method to display car information
     public void displayInfo() {
-        System.out.println("\n--- Car Information ---");
-        System.out.println("Brand: " + brand);
-        System.out.println("Model: " + model);
-        System.out.println("Year: " + year);
-        System.out.println("Color: " + color);
-        System.out.println("Status: " + (isOn ? "On" : "Off"));
-        System.out.println("Speed: " + speed + " km/h");
-        System.out.println("Fuel Level: " + fuelLevel + "%");
-        System.out.println("Mileage: " + mileage + " km");
-        System.out.println("Maintenance Needed: " + (maintenanceNeeded ? "Yes" : "No"));
+        String info = """
+            --- Car Information ---
+            Brand: %s
+            Model: %s
+            Year: %d
+            Color: %s
+            Status: %s
+            Speed: %d km/h
+            Fuel Level: %d%%
+            Mileage: %d km
+            Maintenance Needed: %s
+            """.formatted(
+                brand, model, year, color,
+                isOn ? "On" : "Off",
+                speed, fuelLevel, mileage,
+                maintenanceNeeded ? "Yes" : "No"
+        );
+
+        JOptionPane.showMessageDialog(null, info);
     }
 }
 
-// Derived class SportsCar
+// Classe derivada SportsCar
 class SportsCar extends Car {
     private boolean sportsMode;
 
-    // Constructor
     public SportsCar(String brand, String model, int year, String color) {
         super(brand, model, year, color);
         this.sportsMode = false;
     }
 
-    // Method to activate sports mode
     public void activateSportsMode() {
         if (isOn) {
             sportsMode = true;
-            System.out.println("Sports mode activated! Increased acceleration.");
+            JOptionPane.showMessageDialog(null, "Sports mode activated! Increased acceleration.");
         } else {
-            System.out.println("Cannot activate sports mode while the car is off.");
+            JOptionPane.showMessageDialog(null, "Cannot activate sports mode while the car is off.");
         }
     }
 
-    // Method to deactivate sports mode
     public void deactivateSportsMode() {
         if (sportsMode) {
             sportsMode = false;
-            System.out.println("Sports mode deactivated.");
+            JOptionPane.showMessageDialog(null, "Sports mode deactivated.");
         } else {
-            System.out.println("Sports mode is already off.");
+            JOptionPane.showMessageDialog(null, "Sports mode is already off.");
         }
     }
 
-    // Overriding the displayInfo method to include sports mode
     @Override
     public void displayInfo() {
         super.displayInfo();
-        System.out.println("Sports Mode: " + (sportsMode ? "Activated" : "Deactivated"));
+        JOptionPane.showMessageDialog(null, "Sports Mode: " + (sportsMode ? "Activated" : "Deactivated"));
     }
 }
-
--- Ds-davi-tavares/src/POO/CarTest.java
+++ Ds-davi-tavares/src/POO/CarTest.java
@@ -4,99 +4,69 @@
  */
 package POO;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author DAVI
- */
-// Main class to test the program
 public class CarTest {
-
     public static void main(String[] args) {
-        
-        try (Scanner input = new Scanner(System.in)) {
-            
-            // Ask the user for car details
-            System.out.print("Enter the brand of your car: ");
-            String brand = input.nextLine();
-            
-            System.out.print("Enter the model of your car: ");
-            String model = input.nextLine();
-            
-            System.out.print("Enter the year of your car: ");
-            int year = input.nextInt();
-            input.nextLine();
-            
-            System.out.print("Enter the color of your car: ");
-            String color = input.nextLine();
-            
-            System.out.print("Is your car a sports car? (yes/no): ");
-            String isSportsCar = input.nextLine();
-            Car myCar;
-            
-            if (isSportsCar.equalsIgnoreCase("yes")) {
-                myCar = new SportsCar(brand, model, year, color);
-            } else {
-                myCar = new Car(brand, model, year, color);
-            }   
+        String brand = JOptionPane.showInputDialog("Enter the brand of your car:");
+        String model = JOptionPane.showInputDialog("Enter the model of your car:");
+        int year = Integer.parseInt(JOptionPane.showInputDialog("Enter the year of your car:"));
+        String color = JOptionPane.showInputDialog("Enter the color of your car:");
+        String isSportsCar = JOptionPane.showInputDialog("Is your car a sports car? (yes/no):");
 
-            // Interactive menu
-            int option;
-            do {
-                System.out.println("\n--- MENU ---");
-                System.out.println("1 - Turn on the car");
-                System.out.println("2 - Turn off the car");
-                System.out.println("3 - Accelerate");
-                System.out.println("4 - Brake");
-                System.out.println("5 - Refuel");
-                System.out.println("6 - Perform maintenance");
-                System.out.println("7 - Display car information");
-                if (myCar instanceof SportsCar) {
-                    System.out.println("8 - Activate sports mode");
-                    System.out.println("9 - Deactivate sports mode");
-                }
-                System.out.println("0 - Exit");
-                System.out.print("Choose an option: ");
-                option = input.nextInt();
-
-                switch (option) {
-                    case 1 ->
-                        myCar.turnOn();
-                    case 2 ->
-                        myCar.turnOff();
-                    case 3 -> {
-                        System.out.print("Enter the acceleration value: ");
-                        int increment = input.nextInt();
-                        myCar.accelerate(increment);
-                    }
-                    case 4 ->
-                        myCar.brake();
-                    case 5 -> {
-                        System.out.print("Enter the amount of fuel to refill: ");
-                        int amount = input.nextInt();
-                        myCar.refuel(amount);
-                    }
-                    case 6 ->
-                        myCar.performMaintenance();
-                    case 7 ->
-                        myCar.displayInfo();
-                    case 8 -> {
-                        if (myCar instanceof SportsCar sportsCar) {
-                            sportsCar.activateSportsMode();
-                        }
-                    }
-                    case 9 -> {
-                        if (myCar instanceof SportsCar sportsCar) {
-                            sportsCar.deactivateSportsMode();
-                        }
-                    }
-                    case 0 ->
-                        System.out.println("Exiting the program...");
-                    default ->
-                        System.out.println("Invalid option! Please try again.");
-                }
-            } while (option != 0);
+        Car myCar;
+        if (isSportsCar.equalsIgnoreCase("yes")) {
+            myCar = new SportsCar(brand, model, year, color);
+        } else {
+            myCar = new Car(brand, model, year, color);
         }
-    } // Close the Scanner to prevent resource leaks
+
+        int option;
+        do {
+            String menu = """
+                --- MENU ---
+                1 - Turn on the car
+                2 - Turn off the car
+                3 - Accelerate
+                4 - Brake
+                5 - Refuel
+                6 - Perform maintenance
+                7 - Display car information""" +
+                (myCar instanceof SportsCar ? """
+                \n8 - Activate sports mode
+                9 - Deactivate sports mode""" : "") +
+                "\n0 - Exit";
+            
+            option = Integer.parseInt(JOptionPane.showInputDialog(menu + "\n\nChoose an option:"));
+
+            switch (option) {
+                case 1 -> myCar.turnOn();
+                case 2 -> myCar.turnOff();
+                case 3 -> {
+                    int increment = Integer.parseInt(JOptionPane.showInputDialog("Enter the acceleration value:"));
+                    myCar.accelerate(increment);
+                }
+                case 4 -> myCar.brake();
+                case 5 -> {
+                    int amount = Integer.parseInt(JOptionPane.showInputDialog("Enter the amount of fuel to refill:"));
+                    myCar.refuel(amount);
+                }
+                case 6 -> myCar.performMaintenance();
+                case 7 -> myCar.displayInfo();
+                case 8 -> {
+                    if (myCar instanceof SportsCar sportsCar) {
+                        sportsCar.activateSportsMode();
+                    }
+                }
+                case 9 -> {
+                    if (myCar instanceof SportsCar sportsCar) {
+                        sportsCar.deactivateSportsMode();
+                    }
+                }
+                case 0 -> JOptionPane.showMessageDialog(null, "Exiting the program...");
+                default -> JOptionPane.showMessageDialog(null, "Invalid option! Please try again.");
+            }
+
+        } while (option != 0);
+    }
 }
